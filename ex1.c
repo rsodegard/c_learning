761049c5de51dce9cd49deb47bd8966d5087bbc5
Ryan Odegard <ryanodegard@Ryans-MacBook-Pro.local>
2020-03-06 20:59:06 -0800

    second commit

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// man cc and make cc are good to run for docs
+/* This is a comment. */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+
+	// this is also a comment
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
