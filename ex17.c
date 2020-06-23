15375c4747fca1a359c8cf411a0a29c49e7993c7
Ryan Odegard <ryanodegard@Ryans-MacBook-Pro.local>
2020-06-22 21:09:35 -0700

    commit two june twotwo

--- ex17.c
+++ ex17.c
@@ -73,6 +73,72 @@ struct Connection *Database_open(const char *filename, char mode)
 
 }
 
+void Database_close(struct Connection *conn)
+{
+	if (conn) {
+		if (conn->file)
+			fclose(conn->file);
+		if (conn->db)
+			free(conn->db);
+		free(conn);
+	}
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+		die("USAGE: ex17 <dbfile> <action> [action params]");
+
+	char *filename = argv[1];
+	char action = argv[2][0];
+	struct Connection *conn = Database_open(filename, action);
+	int id = 0;
+
+	if (argc > 3) id = atoi(argv[3]);
+	if (id >= MAX_ROWS) die("There's not that many records.");
+
+	switch (action) {
+		case 'c':
+			Database_create(conn);
+			Database_write(conn);
+			break;
+
+		case 'g':
+			if (argc != 4)
+				die("Need an id to get");
+
+			Database_get(conn, id);
+			break;
+
+		case 's':
+			if (argc != 6)
+				die("Need id, name, email to set");
+
+			Database_set(conn, id, argv[4], argv[5]);
+			Database_write(conn);
+			break;
+
+		case 'd':
+			if (argc != 4)
+				die("Need id to delete");
+
+			Database_delete(conn, id);
+			Database_write(conn);
+			break;
+
+		case 'l':
+			Database_list(conn);
+			break;
+		default:
+			die("Invalid action: c=create, g=get, s=set, d=del, l=list");
+	}
+
+	Database_close(conn);
+
+	return 0;
+}
 
 
 
