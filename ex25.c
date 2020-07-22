ec2c7b46bbf826f844229fcea84549f23fe284f8
Ryan Odegard <snrsdev@gmail.com>
2020-07-21 19:56:10 -0700

    exercise twentyfive commit

---
+++ ex25.c
@@ -0,0 +1,29 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include "dbg.h"
+
+#define MAX_DATA 100
+
+int read_string(char **out_string, int max_buffer)
+{
+    *out_string = calloc(1, max_buffer + 1);
+    check_mem(*out_string);
+
+    char *result = fgets(*out_string, max_buffer, stdin);
+    check(result != NULL, "Input error.");
+
+    return 0;
+
+error:
+    if (*out_string)
+        free(*out_string);
+    *out_string = NULL;
+    return -1
+}
+
+int read_int(int *out_int)
+{
+}
