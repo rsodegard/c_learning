8ee3521cb5508d9fc178b807e4a95c5c7fce0281
Ryan Odegard <snrsdev@gmail.com>
2020-09-11 22:35:59 -0700

    sum worked out

--- modern_c/sumall.c
+++ modern_c/sumall.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+# define MAX 12
 
 
 void die(const char *message)
@@ -13,21 +14,46 @@ void die(const char *message)
 	}
 	exit(1);
 }
+// BELOE IS GOOD FOR ONE NUMBER
+// int array[];
+//
+// typedef struct Sum {
+//   int total;
+// } Sum;
+//
+// Sum this = {.total = 0};
+//
+// int main(int argc, char *argv[])
+// {
+//   if (argc < 1) die("USAGE: ./sumall");
+//
+//   printf("What numbers do you want to add? ");
+//
+//   fscanf(stdin, "%d", &this.total);
+//
+//   printf("%d\n", this.total);
+//
+//   return 0;
+// }
+//ABOVE IS GOOD FOR 1 NUMBER
 
-typedef struct Sum {
-  int total;
-} Sum;
 
-Sum this = {.total = 0};
-
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  if (argc < 1) die("USAGE: ./sumall");
+    int array_of_ints[MAX];
+    int i;
+    int count = argc;
+    int sum = 0;
 
-  printf("What numbers do you want to add? ");
-  fscanf(stdin, "%d", &this.total);
+    for (i = 0; i < argc && i < MAX; i++)
+    {
+        array_of_ints[i] = atoi(argv[i]);
+        printf("%d ", array_of_ints[i]);
+    }
+    for (i = 0; i < count; i++) {
+      sum += array_of_ints[i];
+    }
+      printf("%d\n", sum);
 
-  printf("%d\n", this.total);
-
-  return 0;
+    return 0;
 }
