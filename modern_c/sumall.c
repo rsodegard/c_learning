e355aacaa057dfa9b12b6d1bd043d3292c69314a
Ryan Odegard <snrsdev@gmail.com>
2020-09-11 20:19:29 -0700

    fscanf yep

--- modern_c/sumall.c
+++ modern_c/sumall.c
@@ -14,26 +14,20 @@ void die(const char *message)
 	exit(1);
 }
 
-// int *sum_all(int *numbers, int count)
-// {
-//   for (i = 0; i < count; i++) {
-//
-//   }
-// }
+typedef struct Sum {
+  int total;
+} Sum;
+
+Sum this = {.total = 0};
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2) die("USAGE: ./sumall 5 897 345");
+  if (argc < 1) die("USAGE: ./sumall");
 
-  char *array;
-  int sum = 0;
-  int i = 0;
-  int count = argc;
-  for (i = 0; i < count; i++) {
-    array = argv[i];
-    sum = sum + array;
-    printf("%s\n", array);
-  }
-  printf("%s\n", sum);
+  printf("What numbers do you want to add? ");
+  fscanf(stdin, "%d", &this.total);
+
+  printf("%d\n", this.total);
+
   return 0;
 }
